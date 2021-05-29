a660ae2687709c4264329df6a53dac2975d60aa6
ahsenipekkoca <79426823+engineermom@users.noreply.github.com>
2021-05-30 02:12:48 +0300

    Add project files.

---
+++ WebProje/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebProje.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Kitap()
+        {
+            
+            return View();
+        }
+        public ActionResult Dizi()
+        {
+
+            return View();
+        }
+        public ActionResult Film()
+        {
+
+            return View();
+        }
+    }
+}
